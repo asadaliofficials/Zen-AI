e35dd0c0e3244e984ad4ebd653aa4494268fef61
Asad Ali <asadjattofficials@gmail.com>
2025-10-06 15:01:05 +0500

    setup env

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-import app from "./src/app.js";
+import app from './src/app.js';
 
 
+import { PORT } from './config/env.config.js';
 
-app.listen(PORT, () =>{
-  console.log(`server is listening on http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+	console.log(`server is listening on http://localhost:${PORT}`);
+});
