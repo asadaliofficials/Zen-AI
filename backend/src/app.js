501c4a07cbbfe15e23a2d3c5f3cdd0a5490d42ed
Asad Ali <asadjattofficials@gmail.com>
2025-10-09 22:54:35 +0500

    frontend setup

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+
 import cookieParser from 'cookie-parser';
 
 import authRouter from './routes/auth.route.js';
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AuthPage from './components/AuthPage';
+
+const App = () => {
+	return (
+		<>
+			<AuthPage />
+		</>
+	);
+};
+
+export default App;
