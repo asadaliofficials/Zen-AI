555342026d8e25840f9ba8284db1b7a05f3a86f2
Asad Ali <asadjattofficials@gmail.com>
2025-10-19 17:15:15 +0500

    locked some features for sandbox user

--- frontend/src/components/sandbox/TopBar.sandbox.jsx
+++ frontend/src/components/sandbox/TopBar.sandbox.jsx
@@ -68,7 +68,21 @@ const TopBarSandbox = ({ models = [], selectedModel }) => {
 										}`}
 									>
 										<div className="flex items-center justify-between">
-											<span>{model.name}</span>
+											<span>{model.name}</span>{' '}
+											{model.name === 'zen-1.5' ? (
+												''
+											) : (
+												<svg
+													fill="#000000"
+													width="20px"
+													height="20px"
+													viewBox="-3.5 0 19 19"
+													xmlns="http://www.w3.org/2000/svg"
+													class="cf-icon-svg"
+												>
+													<path d="M11.182 8.927v6.912a.794.794 0 0 1-.792.792H1.61a.794.794 0 0 1-.792-.792V8.927a.794.794 0 0 1 .792-.792h.856V6.367a3.534 3.534 0 1 1 7.068 0v1.768h.856a.794.794 0 0 1 .792.792zm-2.756-2.56a2.426 2.426 0 1 0-4.852 0v1.768h4.852zM7.108 11.47a1.108 1.108 0 1 0-1.583 1.001v1.849a.475.475 0 0 0 .95 0v-1.849a1.108 1.108 0 0 0 .633-1.001z" />
+												</svg>
+											)}
 											{selectedModel === model.id && (
 												<svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
 													<path
